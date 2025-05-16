9b4f1110b669c882a44c9d71438a0b7fff0661bf
NumbersOneThroughJ <152826581+NumbersOneThroughJ@users.noreply.github.com>
2025-05-15 23:05:40 -0400

    MUX update
    not finished still

--- Sudoku Wave Function Colapse/Wave Function Colapse/WFC Algorithm/WFC_MUX.cs
+++ Sudoku Wave Function Colapse/Wave Function Colapse/WFC Algorithm/WFC_MUX.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -83,6 +84,8 @@ namespace Sudoku_Wave_Function_Colapse.Wave_Function_Colapse.WFC_Algorithm
 
     /// <summary>
     /// takes a hash object and converts it to a list of objects
+    /// Currently I do not have the ability to dynamically update the obect
+    ///  With the list of hashes, I might be able to have it recursively update each hash, however that would take a loooot of processing time (relatively a lot)
     /// </summary>
     /// <typeparam name="E"></typeparam>
     internal class WFC_MUX<E> where E : notnull
@@ -93,11 +96,52 @@ namespace Sudoku_Wave_Function_Colapse.Wave_Function_Colapse.WFC_Algorithm
         /// These will allow use of both the strength of lists
         /// and dictionaries as well
         /// </summary>
-        private List<E> outputVariabls;
-        private List<int> weight;
+        private E[] outputVariables;
+        private int[] weight;
+        private List<WFC_MUX_Hash> hashsMade;
         private Dictionary<E, int> outProbDict;
+        //Need a dictionary hash of E to index. This will speed up drastically.
+        private int hashesCount;
 
-        public int COUNT => outputVariabls.Count;
+        public Boolean weighted;
+
+        public int COUNT => outputVariables.Length;
+        public int HashCount => hashesCount;
+
+        private static readonly int[] binaryLocation = new int[] {
+            0b00000000000000000000000000000001,
+            0b00000000000000000000000000000010,
+            0b00000000000000000000000000000100,
+            0b00000000000000000000000000001000,
+            0b00000000000000000000000000010000,
+            0b00000000000000000000000000100000,
+            0b00000000000000000000000001000000,
+            0b00000000000000000000000010000000,
+            0b00000000000000000000000100000000,
+            0b00000000000000000000001000000000,
+            0b00000000000000000000010000000000,
+            0b00000000000000000000100000000000,
+            0b00000000000000000001000000000000,
+            0b00000000000000000010000000000000,
+            0b00000000000000000100000000000000,
+            0b00000000000000001000000000000000,
+            0b00000000000000010000000000000000,
+            0b00000000000000100000000000000000,
+            0b00000000000001000000000000000000,
+            0b00000000000010000000000000000000,
+            0b00000000000100000000000000000000,
+            0b00000000001000000000000000000000,
+            0b00000000010000000000000000000000,
+            0b00000000100000000000000000000000,
+            0b00000001000000000000000000000000,
+            0b00000010000000000000000000000000,
+            0b00000100000000000000000000000000,
+            0b00001000000000000000000000000000,
+            0b00010000000000000000000000000000,
+            0b00100000000000000000000000000000,
+            0b01000000000000000000000000000000,
+            0b1000000000000000000000000000000,
+        };
         #endregion
 
         #region Constructors
@@ -108,35 +152,61 @@ namespace Sudoku_Wave_Function_Colapse.Wave_Function_Colapse.WFC_Algorithm
         /// </summary>
         /// <param name="outputVariabls">Variables to output from a given input</param>
         /// <param name="weight">Integer weight of variable at the same index</param>
-        public WFC_MUX(List<E> outputVariabls, List<int> weight)
+        public WFC_MUX(E[] outputVariabls, int[] weight)
         {
-            this.outputVariabls = outputVariabls;
+            this.outputVariables = outputVariabls;
             this.weight = weight;
             outProbDict = outputVariabls.Zip(weight).ToDictionary(x => x.First, x => x.Second);
+            this.hashesCount = (int)Math.Ceiling(((double)outputVariables.Length)/32);
         }
 
         /// <summary>
         /// Constructs a mutltiplexer with the provided dictionary
         /// </summary>
         /// <param name="outProbDict">Dictionary of values and probabilities</param>
-        public WFC_MUX(Dictionary<E, int> outProbDict)
+        public WFC_MUX(Dictionary<E, int> outProbDict, bool weighted)
         {
             this.outProbDict = outProbDict;
-            this.outputVariabls = outProbDict.Keys.ToList<E>();
-            this.weight = outProbDict.Values.ToList<int>();
+            this.outputVariables = outProbDict.Keys.ToArray<E>();
+            this.weight = outProbDict.Values.ToArray<int>();
+            this.weighted = weighted;
+            this.hashesCount = (int)Math.Ceiling(((double)outputVariables.Length) / 32);
         }
         #endregion
 
         #region functions
 
-        private WFC_MUX_Hash listToHash()
+        /// <summary>
+        /// Creates a multiplexer object based on the source you gave it
+        /// Error logging requires .ToString method of E be defined for better tracing.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private WFC_MUX_Hash listToHash(E[] source, bool weighted)
         {
+            int index = -1;
+
+
+            foreach(E e in source)
+            {
+                index = bitIndexOf(e);
+                if (index == -1) throw new Exception
+                    ("WFC_MUX_Hash does not contain a reference for the specified value: " + e.ToString());
+                //Stopping here, should the hash be calculated in the mux or in the hash object?
+                // I think the MUX should output hash objects. And that would prevent a hash object from being made outside of the mux.
+            }
+
             return new WFC_MUX_Hash();
         }
 
         private List<E> HashToList()
         {
 
+        }
+
+        public int bitIndexOf(E e)
+        {
+            return Array.IndexOf(outputVariables, e);
         }
 
         #endregion
